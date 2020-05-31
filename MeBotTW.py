9ffd1ca34df7830ca41f29dfd98948557a2d15fd
shanglin10 <ggod6108@gmail.com>
2020-06-01 01:54:17 +0800

    update0601
    bot online.會員進入/離開.ping查詢

--- MeBotTW.py
+++ MeBotTW.py
@@ -1,2 +1,26 @@
-Hello
-Test
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='[')
+
+@bot.event
+async def on_ready():
+    print(">> Bot is online <<")
+
+@bot.event
+async def on_member_join(member):
+    print(f'{member} join')
+    channel = bot.get_channel(716689928553103380)
+    await channel.send(f'{member} join ')
+
+@bot.event
+async def on_member_remove(member):
+    print(f'{member} leave')
+    channel = bot.get_channel(716689928553103380)
+    await channel.send(f'{member} leave  ')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency*1000)}(ms)')
+
+bot.run('NzE0ODM4NTc4NTg4Mjg3MTE3.XtPPLg.oHlKz5uS35DkGKsZQYRIII16dfw')
